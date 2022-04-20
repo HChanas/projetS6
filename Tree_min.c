01e45aed615c6c0c100d59ad6b27db0b68a2249e
Amaury <amaurylange@gmail. com>
2022-04-20 17:26:41 +0200

    INIT noued et nouveau files

--- Tree_min.c
+++ Tree_min.c
@@ -1,105 +1,41 @@
+#include "jeu.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#define NB_FILS_MAX T_PLAT / 2
 
 typedef struct Noeud Noeud;
 struct Noeud {
-  int nombre_de_points; // stocke le resultat pour les calculs
-  int numero_move;
+  int valeur;  // Si le noeud est une feille ou si la valeur de ses fils a été
+               // évaluée. Valeur donnée initialement aux feuilles par la
+               // fonction d'évaluation.
+  int feuille; // Indique si le noeud est une feuille pour simplifier des trucs
+  int coups;
   int numero_joeur;
-  Noeud *suivant; // adresse de l'element suivant dans la liste
-  Noeud *un;
-  Noeud *deux;
-  Noeud *trois;
-  Noeud *quatre;
-  Noeud *cinq;
-  Noeud *six;
+  Noeud *fils[NB_FILS_MAX];
+  // Tableau de pointeurs vers les fils. Il peut y avoir
+  // des NULL, si certains coups étaient impossibles.
+  // A la i-ème case du tableau, se trouve l'adresse du noeud correspondant au
+  // coup i(+1). e.g. fils[1] -> trou 2 (ou 8 si joueur 2). Si par exemple le
+  // trou 4 (ou 10 donc) ne peut être joué, fils[3] = NULL.
 };
 
-typedef struct Liste Liste;
-struct Liste {
-  Noeud *head;   // stocke le debut de la liste
-  Noeud *tail;   // stocke la fin de la liste;
-  Noeud *racine; // stocke la racine de l'arbre
-};
+/* Initialise les champs du noeud (fils à NULL et valeur à 0 par défaut). */
+void init_noeud(Noeud *nouveau) {
 
-Liste *initialisation() {
-  Liste *liste = malloc(sizeof(*liste));
-
-  if (liste == NULL) {
-    exit(EXIT_FAILURE);
+  nouveau->valeur = 0;
+  nouveau->feuille = 0;
+  nouveau->coups = 0;
+  nouveau->numero_joeur = 0;
+  for (int i = 0; i < NB_FILS_MAX; i++) {
+    nouveau->fils[i] = NULL;
   }
-  liste->head = liste->tail = NULL;
-  return liste;
 }
-// ajoute un element de la liste a la fin de la chaine
-void insertion(Liste *liste, int numero_move, int numero_joeur) {
+/* Crée un nouveau noeud (initialisé) et met son adresse dans la case du tableau
+ * fils donnée. L'adresse du nouveau noeud est renvoyée. */
+Noeud *nouveau_fils(Noeud *n, int indice) {
   Noeud *nouveau = malloc(sizeof(*nouveau));
-  if (liste == NULL || nouveau == NULL) {
-    exit(EXIT_FAILURE);
-  }
-  nouveau->numero_move = numero_move;
-  nouveau->numero_joeur = numero_joeur;
-  nouveau->un = NULL;
-  nouveau->deux = NULL;
-  nouveau->trois = NULL;
-  nouveau->quatre = NULL;
-  nouveau->cinq = NULL;
-  nouveau->six = NULL;
-  nouveau->suivant = NULL;
-
-  if (nouveau == NULL) {
-    printf("not initialized\n");
-    free(nouveau);
-  } else if (NULL == liste->head && NULL == liste->tail) {
-
-    liste->head = liste->tail = nouveau;
-  } else if (NULL == liste->head || NULL == liste->tail) {
-    fprintf(stderr, "ERROR head ou tail vide\n");
-    free(nouveau);
-  } else {
-    /* printf("List not empty, adding element to tail\n"); */
-    liste->tail->suivant = nouveau;
-    liste->tail = nouveau;
-  }
-}
-
-// enleve un element de la liste a la fin
-void list_remove_element(Liste *liste) {
-  Noeud *h = NULL;
-  Noeud *p = NULL;
-
-  if (NULL == liste) {
-    printf("List is empty\n");
-  } else if (NULL == liste->head && NULL == liste->tail) {
-    printf("Well, List is empty\n");
-  } else if (NULL == liste->head || NULL == liste->tail) {
-    printf("There is something seriously wrong with your list\n");
-    printf("One of the head/tail is empty while other is not \n");
-  }
-
-  h = liste->head;
-  p = h->suivant;
-  free(h);
-  liste->head = p;
-}
-// supprime jusqu'a ce que la liste soit vide
-void suppression_chaine(Liste *liste) {
-  while (liste->head != NULL) {
-    list_remove_element(liste);
-  }
-  free(liste);
-}
-
-// Liste *nouvel_arbre(int *plateau, int joueur, int profondeur) {}
-
-int main(int argc, char *argv[]) {
-  int resultat;
-  Liste *liste = initialisation();
-
-  /* Génère un arbre de possibilités à partir d'un plateau de jeu et du joueur
-   * qui va jouer. La profondeur de l'arbre est précisée. */
+  init_noeud(nouveau);
+  n->fils[indice] = nouveau;
+  return nouveau;
 }
